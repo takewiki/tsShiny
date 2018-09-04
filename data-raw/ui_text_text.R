d18debede2283c72ad5219dcd1bb868f964740f9
takewiki <hulilei@takewiki.com.cn>
2018-09-04 08:01:21 +0800

    add test sample for shiny_inut on ui_number_slider

--- data-raw/ui_text_text.R
+++ data-raw/ui_text_text.R
@@ -2,6 +2,6 @@ bb <- ui_text(inputId = "aa",label = "bb",value = "cc",width = "80%");
 shiny_input(bb);
 class(bb);
 
-cc <-ui_text();
-cc;
-shiny_input(cc);
+ins_ui_number_slider <- ui_number_slider();
+shiny_input(ins_ui_number_slider);
+
